0e163e84a5db1fce7c74bb0af1664e39201a2236
hanoelleb <hanoelleb@gmail.com>
2020-08-16 13:51:41 -0700

    can get tokens from login

--- src/Auth.js
+++ src/Auth.js
@@ -9,9 +9,70 @@ class Auth extends React.Component {
     render() {
         return (
 	    <div>
-		Auth
+		<button onClick={()=>this.setState({menuStatus: 0})}>
+		    Login
+		</button>
+
+		<button onClick={()=>this.setState({menuStatus: 1})}>
+		    Register
+		</button>
+
+		{ this.state.menuStatus == 0 ? <Login /> : < Register/> }
             </div>
 	)
+    }
+}
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+	this.handleChange = this.handleChange.bind(this);
+	this.handleSubmit = this.handleSubmit.bind(this);
+	this.state = ({name: "", password: ""});
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.name] : event.target.value });
+    }
+
+    handleSubmit(event) {
+	event.preventDefault();
+	const credentials = { name: this.state.name, 
+		                  password: this.state.password };
+        fetch('http://hanoelleb-forme.herokuapp.com/api/auth/login',
+            { 
+                method: 'POST',
+		headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                body: new URLSearchParams(credentials),
+                json: true
+            })
+	     .then( response => response.json())
+	     .then( data => console.log(data));
+    }
+
+    render() {
+        return (
+	    <form onSubmit={this.handleSubmit}>
+		<input type='text' name='name' onChange={this.handleChange} 
+		    placeholder='name' />
+		<input type='password' name='password' 
+		    onChange={this.handleChange} placeholder='password' />
+		<input type='submit' value='Login' />
+            </form>
+	)
+    }
+}
+
+class Register extends React.Component {
+    render() {
+       return (
+         <form>
+             <input type='text' placeholder='name' />
+             <input type='email' placeholder='email' />
+             <input type='password' placeholder='password' />
+             <input type='submit' value='Create Account' />
+         </form>
+       )
     }
 }
 
