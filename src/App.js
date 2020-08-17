c1d81d10dc43826b8197e3b5f83a830e7876b317
hanoelleb <hanoelleb@gmail.com>
2020-08-17 01:12:18 -0700

    started styling

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
    return (
     <div className="App">
       <header className="App-header">
+	  <h2>Forme</h2>
 	  { this.state.token === '' ? < Auth handler={this.tokenHandler} /> 
             : <div>
                   <button onClick={this.logout}>Logout</button>
