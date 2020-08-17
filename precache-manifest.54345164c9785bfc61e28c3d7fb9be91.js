3648b0cf2e3476f279ad57e24d1564d316d487bb
hanoelleb <hanoelleb@gmail.com>
2020-08-17 01:15:02 -0700

    Updates

---
+++ precache-manifest.54345164c9785bfc61e28c3d7fb9be91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8dbbca19a9cdba14077135fec4412a",
+    "url": "/forme-front/index.html"
+  },
+  {
+    "revision": "d2daffef8025b43372db",
+    "url": "/forme-front/static/css/main.a30b462a.chunk.css"
+  },
+  {
+    "revision": "d69f7f18b1ff1ade24cc",
+    "url": "/forme-front/static/js/2.8b9feadb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/forme-front/static/js/2.8b9feadb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2daffef8025b43372db",
+    "url": "/forme-front/static/js/main.ae16281c.chunk.js"
+  },
+  {
+    "revision": "5ed3ee61a48fd28f7767",
+    "url": "/forme-front/static/js/runtime-main.ee6596db.js"
+  }
+]);
